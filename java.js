6bfbe9d9ab38ab86c6f4be51c5df69ec71254a82
Franco Echaide <echaidefranco@gmail.com>
2023-09-04 21:23:33 -0300

    ssss

---
+++ java.js
@@ -0,0 +1,26 @@
+let enviar = document.getElementById("enviar");
+
+async function enviarDatos(nombre, apellido, nac) {
+	const persona = { nombre, apellido, nac };
+
+	const response = await fetch("https://jsonplaceholder.typicode.com/users", {
+		method: "POST",
+
+		body: JSON.stringify(persona),
+
+		headers: {
+			"Content-type": "application/json",
+		},
+	});
+	if (response.ok) {
+		const jsonData = await response.json();
+		console.log(jsonData);
+	}
+}
+enviar.addEventListener("click", function () {
+	let nombre = document.getElementById("nombre").value;
+	let apellido = document.getElementById("apellido").value;
+	let nac = document.getElementById("fechaNac").value;
+
+	enviarDatos(nombre, apellido, nac);
+});
